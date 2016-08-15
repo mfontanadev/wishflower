409086b122659915066bb371497fbd472cffbd31
mfontana <mfontanadev@gmail.com>
2016-08-15 15:44:23 -0300

    Grass and log in ladybug game cycle

--- controllers/activities/wishflowerLadybugTestActivity.js
+++ controllers/activities/wishflowerLadybugTestActivity.js
@@ -8,6 +8,11 @@ function WishflowerLadybugTestActivity(_id, _viewParent)
 	this.m_viewParent = _viewParent; 
 
 	this.m_ladybug = null;
+	this.m_grass_img = null;
+	this.m_log_img = null;
+
+	this.m_grass_x = 0;
+	this.m_grass_y = 0;
 };
 
 WishflowerLadybugTestActivity.prototype.initialize = function ()
@@ -16,6 +21,12 @@ WishflowerLadybugTestActivity.prototype.initialize = function ()
 
 	this.m_ladybug = new Ladybug();
 	this.m_ladybug.initWithType(this.m_viewParent, Ladybug.C_LADYBUG_TYPE_WISHMASTER);
+
+    this.m_grass_img = this.m_viewParent.getBitmapManagerInstance().getImageByName('ctree_root3.png');
+    this.m_log_img = this.m_viewParent.getBitmapManagerInstance().getImageByName('log.png');
+
+
+	console.log(this.m_log_img.height);
 
 	this.createControls();
 };
@@ -42,6 +53,21 @@ WishflowerLadybugTestActivity.prototype.render = function ()
 {
 	this.m_ladybug.render();
 
+
+    drawImageRotationTransparentScaled( this.m_viewParent.m_canvasEx.m_canvas, 
+                                    	this.m_viewParent.m_canvasEx.m_context, 
+                                    	this.m_grass_img, 
+                                    	this.m_viewParent.m_canvasEx.m_canvas.width / 2, 
+                                    	this.m_viewParent.m_canvasEx.m_canvas.height, 
+                                    	0, 0.7, 0.9);
+    drawImageRotationTransparentScaled( this.m_viewParent.m_canvasEx.m_canvas, 
+                                    	this.m_viewParent.m_canvasEx.m_context, 
+                                    	this.m_log_img, 
+                                    	this.m_viewParent.m_canvasEx.m_canvas.width / 2, 
+                                    	this.m_viewParent.m_canvasEx.m_canvas.height - ((this.m_log_img.height / 5) * 0.2), 
+                                    	0, 1, 0.2);
+xº
+    //console.log(this.m_viewParent.m_canvasEx.m_canvasHeight);
 	this.renderControls();
 };
 
--- controllers/entities/ladybug.js
+++ controllers/entities/ladybug.js
@@ -4,14 +4,6 @@ Ladybug.C_LADYBUG_TYPE_NOT_SET = 0;
 Ladybug.C_LADYBUG_TYPE_WISHMASTER = 1;
 Ladybug.C_LADYBUG_TYPE_ABOUT = 2;
 
-Ladybug.ladybug_img = null;
-Ladybug.ladybug_elytra_img = null;
-Ladybug.ladybug_under_img = null;
-Ladybug.ladybug_walkLeft_img = null;
-Ladybug.ladybug_walkRight_img = null;
-Ladybug.ladybug_wing_img = null;
-Ladybug.log_img = null;
-
 function Ladybug() 
 {
     this.m_viewParent = null;
@@ -22,7 +14,15 @@ function Ladybug()
 
     this.m_transparent = 1;
     this.m_angle = 30;
-    this.m_scale = 0.2;
+    this.m_scale = 0.15;
+
+    this.m_img = null;
+    this.m_elytra_img = null;
+    this.m_under_img = null;
+    this.m_walkLeft_img = null;
+    this.m_walkRight_img = null;
+    this.m_wing_img = null;
+    this.m_log_img = null;
 
     Ladybug.prototype.initWithType = function (_viewParent, _ladyBugType) 
     {
@@ -37,13 +37,13 @@ function Ladybug()
 
     Ladybug.prototype.loadImages = function () 
     {
-        Ladybug.ladybug_img = this.m_viewParent.getBitmapManagerInstance().getImageByName('ladybug.png');
-        Ladybug.ladybug_elytra_img = this.m_viewParent.getBitmapManagerInstance().getImageByName('ladybug_elytra.png');
-        Ladybug.ladybug_under_img = this.m_viewParent.getBitmapManagerInstance().getImageByName('ladybug_under.png');
-        Ladybug.ladybug_walkLeft_img = this.m_viewParent.getBitmapManagerInstance().getImageByName('ladybug_walk_left.png');
-        Ladybug.ladybug_walkRight_img = this.m_viewParent.getBitmapManagerInstance().getImageByName('ladybug_walk_right.png');
-        Ladybug.ladybug_wing_img = this.m_viewParent.getBitmapManagerInstance().getImageByName('ladybug_wing.png');
-        Ladybug.log_img = this.m_viewParent.getBitmapManagerInstance().getImageByName('log.png');
+        this.m_img = this.m_viewParent.getBitmapManagerInstance().getImageByName('ladybug.png');
+        this.m_elytra_img = this.m_viewParent.getBitmapManagerInstance().getImageByName('ladybug_elytra.png');
+        this.m_under_img = this.m_viewParent.getBitmapManagerInstance().getImageByName('ladybug_under.png');
+        this.m_walkLeft_img = this.m_viewParent.getBitmapManagerInstance().getImageByName('ladybug_walk_left.png');
+        this.m_walkRight_img = this.m_viewParent.getBitmapManagerInstance().getImageByName('ladybug_walk_right.png');
+        this.m_wing_img = this.m_viewParent.getBitmapManagerInstance().getImageByName('ladybug_wing.png');
+        this.m_log_img = this.m_viewParent.getBitmapManagerInstance().getImageByName('log.png');
     };
 
     Ladybug.prototype.handleInputs = function () 
@@ -70,7 +70,7 @@ function Ladybug()
     {
         drawImageRotationTransparentScaled( this.m_viewParent.m_canvasEx.m_canvas, 
                                             this.m_viewParent.m_canvasEx.m_context, 
-                                            Ladybug.ladybug_img, 
+                                            this.m_img, 
                                             this.m_cx, this.m_cy, this.m_angle, this.m_transparent, this.m_scale);
     };
 
