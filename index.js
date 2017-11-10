71ff144bc8e079d424393f243817f2734b70a682
Mauricio Fontana Lab <mfontana@baufest.com>
2017-11-10 13:32:07 -0300

    minor hange, index.js: identation of some sectoins.

--- index.js
+++ index.js
@@ -3,128 +3,139 @@ var app = express();
 var port = (process.env.PORT || 5000 );
 var url = require('url');
 
-// config
-var C_USE_MONGODB_OLD_CONNECTION_TYPE = false;
-var C_USE_MLAB_DATABASE_HOST = true;
+// Config
+{
+	var C_USE_MONGODB_OLD_CONNECTION_TYPE = false;
+	var C_USE_MLAB_DATABASE_HOST = true;
 
-// Global definitions
-global.__basePath = __dirname;
-global.__mockDB = false;
-global.__dbClient = null;
-global.__services = null;
+	// Global definitions
+	global.__basePath = __dirname;
+	global.__mockDB = true;
+	global.__dbClient = null;
+	global.__services = null;
 
-var reqGlobals = require('./config/globalDefinitions.js'); 
-global.__configDefinitions = new reqGlobals();
+	var reqGlobals = require('./config/globalDefinitions.js'); 
+	global.__configDefinitions = new reqGlobals();
+}
 
 // Entry point.
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + '/views/' + 'wishflowerView.html');
-});
+{
+	app.get("/", function (req, res) {
+	    res.sendFile(__dirname + '/views/' + 'wishflowerView.html');
+	});
 
-app.get ('/favicon.ico', function (req, res) {
-		res.send('/public/favicon.ico');
-});
+	app.get ('/favicon.ico', function (req, res) {
+			res.send('/public/favicon.ico');
+	});
+}
 
 // Routes
-require('./views')(app);
-require('./controllers')(app);
-require('./models')(app);
+{
+	require('./views')(app);
+	require('./controllers')(app);
+	require('./models')(app);
 
-require('./lib/chel-dist')(app);
-require('./lib/sound-0.6.1-dist/js')(app);
-require('./lib/jquery-1.11.2-min/js')(app);
-require('./config')(app);
+	require('./lib/chel-dist')(app);
+	require('./lib/sound-0.6.1-dist/js')(app);
+	require('./lib/jquery-1.11.2-min/js')(app);
+	require('./config')(app);
 
-app.use(express.static(__dirname + '/public/assets/img'));
-app.use(express.static(__dirname + '/public/assets/snd'));
+	app.use(express.static(__dirname + '/public/assets/img'));
+	app.use(express.static(__dirname + '/public/assets/snd'));
+}
 
 // SERVICES
-var reqServiceRoute = require('./controllers/services/wishflower.Route.js');
-global.__services = new reqServiceRoute(app); 
+{
+	var reqServiceRoute = require('./controllers/services/wishflower.Route.js');
+	global.__services = new reqServiceRoute(app); 
+}
 
 // Database 
-console.log('');
-console.log('Database initializing.');
-if (global.__mockDB === false)
 {
-	var mongodb = require('mongodb');
-
-	if (C_USE_MONGODB_OLD_CONNECTION_TYPE === true)
+	console.log('');
+	console.log('Database initializing.');
+	if (global.__mockDB === false)
 	{
-		var server = new mongodb.Server("127.0.0.1", 27017, {});
-		var dbBaseTest = new mongodb.Db('wishflowerdb', server, {});
-		dbBaseTest.open
-		(
-			function (error, client) 
-			{
-				global.__dbClient = client;
+		var mongodb = require('mongodb');
 
-				if (typeof global.__dbClient !== 'undefined' && global.__dbClient !== null)
-				{
-					global.__services.getInstance().init(global.__dbClient);
-					console.log('Database initialized.');
-				}
-				else
-				{
-					console.log('Database error:' + error);
-				}
-			}
-		);
-	}
-	else
-	{
-		var mongoClient = mongodb.MongoClient;
-		var urlDB = "";
-
-		if (C_USE_MLAB_DATABASE_HOST === true)
+		if (C_USE_MONGODB_OLD_CONNECTION_TYPE === true)
 		{
-			urlDB = "mongodb://wishflowerdb_admin:wishflowerdb_pass@ds147421.mlab.com:47421/wishflowerdb"
+			var server = new mongodb.Server("127.0.0.1", 27017, {});
+			var dbBaseTest = new mongodb.Db('wishflowerdb', server, {});
+			dbBaseTest.open
+			(
+				function (error, client) 
+				{
+					global.__dbClient = client;
+
+					if (typeof global.__dbClient !== 'undefined' && global.__dbClient !== null)
+					{
+						global.__services.getInstance().init(global.__dbClient);
+						console.log('Database initialized.');
+					}
+					else
+					{
+						console.log('Database error:' + error);
+					}
+				}
+			);
 		}
 		else
 		{
-			urlDB = "mongodb://localhost:27017/wishflowerdb"
-		}
+			var mongoClient = mongodb.MongoClient;
+			var urlDB = "";
 
-		mongoClient.connect
-		(
-			urlDB, 
-			function (error, client) 
+			if (C_USE_MLAB_DATABASE_HOST === true)
 			{
-				global.__dbClient = client;
+				urlDB = "mongodb://wishflowerdb_admin:wishflowerdb_pass@ds147421.mlab.com:47421/wishflowerdb"
+			}
+			else
+			{
+				urlDB = "mongodb://localhost:27017/wishflowerdb"
+			}
 
-				if (typeof global.__dbClient !== 'undefined' && global.__dbClient !== null)
+			mongoClient.connect
+			(
+				urlDB, 
+				function (error, client) 
 				{
-					global.__services.getInstance().init(global.__dbClient);
-					console.log('Database initialized. Connection established to ' + urlDB);
+					global.__dbClient = client;
+
+					if (typeof global.__dbClient !== 'undefined' && global.__dbClient !== null)
+					{
+						global.__services.getInstance().init(global.__dbClient);
+						console.log('Database initialized. Connection established to ' + urlDB);
+					}
+					else
+					{
+						console.log('Database error:' + error);
+					}
 				}
-				else
-				{
-					console.log('Database error:' + error);
-				}
+			);
+		}
+	}
+	else
+	{
+		global.__services.getInstance().init(null);
+		console.log('Database initialized (mock).');
+	}
+}
+
+// Start socket listening.
+{
+	console.log('');
+	var io = require('socket.io').listen(app.listen(port));
+	console.log("SOCKETIO: listening on port " + port );
+
+	if (typeof io === 'undefined')
+	{
+		app.listen
+		(
+			port, 
+			function() 
+			{
+				console.log('LISTEN: listening on port ' + port);
 			}
 		);
 	}
 }
-else
-{
-	global.__services.getInstance().init(null);
-	console.log('Database initialized (mock).');
-}
-
-// Start socket listening.
-console.log('');
-var io = require('socket.io').listen(app.listen(port));
-console.log("SOCKETIO: listening on port " + port );
-
-if (typeof io === 'undefined')
-{
-	app.listen
-	(
-		port, 
-		function() 
-		{
-			console.log('LISTEN: listening on port ' + port);
-		}
-	);
-}
-
